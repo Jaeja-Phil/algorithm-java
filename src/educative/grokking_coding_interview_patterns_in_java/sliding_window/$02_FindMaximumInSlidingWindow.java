e7c46425d8ed1ed5e814d2a872af7a8bbd634447
phil <jaeja.phil@gmail.com>
2023-04-10 20:24:52 +0900

    feat(Sliding Window): find maximum in sliding window, add deque implementation

--- src/educative/grokking_coding_interview_patterns_in_java/sliding_window/$02_FindMaximumInSlidingWindow.java
+++ src/educative/grokking_coding_interview_patterns_in_java/sliding_window/$02_FindMaximumInSlidingWindow.java
@@ -1,9 +1,6 @@
 package educative.grokking_coding_interview_patterns_in_java.sliding_window;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.PriorityQueue;
+import java.util.*;
 
 /**
  * Given an integer list, nums, find the maximum values in all the contiguous subarrays (windows) of size w.
@@ -28,6 +25,9 @@ public class $02_FindMaximumInSlidingWindow {
     public static void main(String[] args) {
         System.out.println(Arrays.toString(solution(new int[] {-4, 5, 4, -4, 4, 6, 7, 20}, 2))); // [5, 5, 4, 4, 6, 7, 20]
         System.out.println(Arrays.toString(solution(new int[] {-4, 5, 4, -4, 4, 6, 7}, 10))); // [7]
+        // same using solution2
+        System.out.println(Arrays.toString(solution2(new int[] {-4, 5, 4, -4, 4, 6, 7, 20}, 2))); // [5, 5, 4, 4, 6, 7, 20]
+        System.out.println(Arrays.toString(solution2(new int[] {-4, 5, 4, -4, 4, 6, 7}, 10))); // [7]
     }
 
     public static int[] solution(int[] nums, int w) {
@@ -67,6 +67,55 @@ public class $02_FindMaximumInSlidingWindow {
         if (answer.size() == 0) {
             // add the maximum value in the array to the answer list
             answer.add(maxHeap.peek().val);
+        }
+
+        // return the answer list as int[]
+        return answer.stream().mapToInt(i -> i).toArray();
+    }
+
+    /**
+     * change the above solution's maxHeap to deque
+     */
+    public static int[] solution2(int[] nums, int w) {
+        // constraint: 1 <= arr.length <= 1000, so no need to check the length == 0 of nums
+
+        // initialize the left & right pointers
+        int left = 0, right = 0;
+
+        // create deque to store the maximum value in the sliding window
+        // use node class with index to remove the value that is out of the sliding window
+        Deque<Node> deque = new LinkedList<>();
+
+        // create answer list
+        List<Integer> answer = new ArrayList<>();
+
+        // iterate through the nums array
+        for (int i = 0; i < nums.length; i++) {
+            // add the current value to the deque
+            while (!deque.isEmpty() && deque.peekLast().val < nums[i]) {
+                deque.pollLast();
+            }
+            deque.offerLast(new Node(nums[i], i));
+
+            // expand right side of the sliding window
+            right++;
+            // when the sliding window reaches the size of w...
+            if (right - left == w) {
+                // remove the value that is out of the sliding window
+                while (deque.peekFirst().idx < left) {
+                    deque.pollFirst();
+                }
+                // add the maximum value in the sliding window to the answer list
+                answer.add(deque.peekFirst().val);
+                // shrink left side of the sliding window
+                left++;
+            }
+        }
+
+        // answer of size 0 means that the window size is greater than the length of the array
+        if (answer.size() == 0) {
+            // add the maximum value in the array to the answer list
+            answer.add(deque.peekFirst().val);
         }
 
         // return the answer list as int[]
